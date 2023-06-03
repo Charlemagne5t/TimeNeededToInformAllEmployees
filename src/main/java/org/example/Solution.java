238f65d4798381f78a6e9d4a2f7379c00d3632da
alek <wait82985@gmail.com>
2023-06-03 15:17:51 +0600

    alogo seems to work 33/39 test cases passed but TLE. Memoization needed

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,25 @@ package org.example;
 
 public class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        //TODO
-        return  0;
+
+        int i = 0;
+        for ( ; i < manager.length; i++) {
+            if(manager[i] == -1) break;
+        }
+        int[] max = {0};
+        boolean[] visited = new boolean[n];
+        return dfs( i, manager, informTime, max, 0, visited);
     }
+
+    private int dfs(int headID, int[] manager, int[] informTime, int[] max, int pathValue, boolean[] visited) {
+        visited[headID] = true;
+        for (int i = 0; i < manager.length; i++) {
+            if(manager[i] == headID && !visited[i]){
+                max[0] = dfs(i, manager, informTime, max, pathValue + informTime[headID], visited);
+            }
+        }
+        return Math.max(max[0], pathValue);
+    }
+
 }
+
